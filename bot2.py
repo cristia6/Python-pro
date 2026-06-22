d1ed10a67c40a1bd0e25c99bf465453db09451c7
cristia6 <2010cristianocarbone@gmail.com>
2026-06-21 21:39:45 -0300

    Add basic Discord bot with commands
    Uma cópia do outro bot ,mas usando bot ao invés de client

---
+++ bot2.py
@@ -0,0 +1,26 @@
+import discord
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Fizemos login como {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send("Hello!")
+
+@bot.command()
+async def bye(ctx):
+    await ctx.send("\U0001f642")
+
+
+@bot.command()
+async def echo(ctx, *, message):
+    await ctx.send(message)
+
+bot.run("COLOQUE SEU TOKEN AQUI")
