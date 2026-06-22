6d563af26beff45924ac68da23744be29a78adc4
cristia6 <2010cristianocarbone@gmail.com>
2026-06-21 22:22:06 -0300

    Update bot2.py
    Ajustei as funções para os comandos e melhorei a senha aleatória , agora podendo escolher a quantia de algarismos

--- bot2.py
+++ bot2.py
@@ -1,5 +1,10 @@
 import discord
+import random
 from discord.ext import commands
+
+from bot_logic import gen_pass
+
+from bot_logic import caraoucoroa
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -18,6 +23,18 @@ async def hello(ctx):
 async def bye(ctx):
     await ctx.send("\U0001f642")
 
+@bot.command()
+async def melhorjogo(ctx):
+    await ctx.send("hollow knight")
+
+@bot.command()
+async def gerarsenha(ctx , tamanho = 10):
+    senha = gen_pass(tamanho)
+    await ctx.send(f"Senha gerada: {senha}")
+
+@bot.command()
+async def caraoucoroa(ctx):
+    await ctx.send(caraoucoroa())   
 
 @bot.command()
 async def echo(ctx, *, message):
